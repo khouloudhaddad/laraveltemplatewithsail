c0fedcfa2d5d58baf8a9eab7276c370a49b8f136
khouloudhaddad <khouloud.haddad@gmail.com>
2023-05-15 00:56:46 +0100

    Register handle submit

--- react/src/views/Register.jsx
+++ react/src/views/Register.jsx
@@ -1,15 +1,45 @@
+import { useRef } from "react";
 import { Link } from "react-router-dom";
+import axiosClient from '../axiosClient';
+import { useStateContext } from "../contexts/ContextProvider";
+
 const Register = () => {
+
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmationRef = useRef();
+
+  const {setUser, setToken} = useStateContext()
+
     const handleSubmit = (e) => {
         e.preventDefault();
+        const payload = {
+          name: nameRef.current.value,
+          email: emailRef.current.value,
+          password: passwordRef.current.value,
+          password_confirmation: passwordConfirmationRef.current.value
+        }
+        console.log(payload)
+        axiosClient.post('/register', payload)
+        .then(({data})=>{
+          setUser(data.user)
+          setToken(data.token)
+        })
+        .catch(err=>{
+          const response = err.response;
+          if(response && response.status === 422){
+            console.log(response.data.errors)
+          }
+        })
     };
     return (
         <form onSubmit={handleSubmit}>
             <h1 className="title">Create an account</h1>
-            <input type="text" placeholder="Full Name" />
-            <input type="email" placeholder="Email Address" />
-            <input type="password" placeholder="Password" />
-            <input type="password" placeholder="Password Confirmation" />
+            <input ref={nameRef} type="text" placeholder="Full Name" />
+            <input ref={emailRef} type="email" placeholder="Email Address" />
+            <input ref={passwordRef} type="password" placeholder="Password" />
+            <input ref={passwordConfirmationRef} type="password" placeholder="Password Confirmation" />
             <button type="submit" className="btn btn-block">
                 Signup
             </button>
