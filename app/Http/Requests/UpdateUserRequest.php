6e2afba27bc27916aa495339e7ff0670bba068ee
khouloudhaddad <khouloud.haddad@gmail.com>
2023-05-16 15:25:20 +0100

    Update user

--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -24,7 +24,7 @@ class UpdateUserRequest extends FormRequest
     {
         return [
             "name" => 'required|string|max:55',
-            "email" =>'required|email|unique:users,email'.$this->id,
+            "email" =>'required|email|unique:users,email,'.$this->id,
             "password" =>[
                 'confirmed',
                 Password::min(8)
--- react/src/views/UserForm.jsx
+++ react/src/views/UserForm.jsx
@@ -1,8 +1,113 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import axiosClient from "../axiosClient";
 
 const UserForm = () => {
-  return (
-    <div>UserForm</div>
-  )
-}
+    const navigate = useNavigate();
+    const { id } = useParams();
+    const [loading, setLoading] = useState(false);
+    const [errors, setErrors] = useState(null);
+    const [user, setUser] = useState({
+        id: null,
+        name: "",
+        email: "",
+        password: "",
+        password_confirmation: "",
+    });
 
-export default UserForm
+    if (id) {
+        useEffect(() => {
+            setLoading(true);
+            axiosClient(`/users/${id}`)
+                .then(({ data }) => {
+                    setLoading(false);
+                    setUser(data.data);
+                })
+                .catch(() => {
+                    setLoading(false);
+                });
+        }, []);
+    }
+
+    const onSubmit = ev => {
+        ev.preventDefault()
+        if (user.id) {
+          axiosClient.put(`/users/${user.id}`, user)
+            .then(() => {
+              navigate('/users')
+            })
+            .catch(err => {
+              const response = err.response;
+              if (response && response.status === 422) {
+                setErrors(response.data.errors)
+              }
+            })
+        } else {
+          axiosClient.post('/users', user)
+            .then(() => {
+              navigate('/users')
+            })
+            .catch(err => {
+              const response = err.response;
+              if (response && response.status === 422) {
+                setErrors(response.data.errors)
+              }
+            })
+        }
+      }
+
+    return (
+        <>
+            {user.id && <h1>Update User: {user.name}</h1>}
+            {!user.id && <h1>New User</h1>}
+            <div className="card animated fadeInDown">
+                {loading && <div className="text-center">Loading...</div>}
+                {errors && (
+                    <div className="alert">
+                        {Object.keys(errors).map((key) => (
+                            <p key={key}>{errors[key][0]}</p>
+                        ))}
+                    </div>
+                )}
+                {!loading && (
+                    <form onSubmit={onSubmit}>
+                        <input
+                            value={user.name}
+                            onChange={(ev) =>
+                                setUser({ ...user, name: ev.target.value })
+                            }
+                            placeholder="Name"
+                        />
+                        <input
+                            value={user.email}
+                            onChange={(ev) =>
+                                setUser({ ...user, email: ev.target.value })
+                            }
+                            placeholder="Email"
+                        />
+                        <input
+                            type="password"
+                            onChange={(ev) =>
+                                setUser({ ...user, password: ev.target.value })
+                            }
+                            placeholder="Password"
+                        />
+                        <input
+                            type="password"
+                            onChange={(ev) =>
+                                setUser({
+                                    ...user,
+                                    password_confirmation: ev.target.value,
+                                })
+                            }
+                            placeholder="Password Confirmation"
+                        />
+                        <button className="btn">Save</button>
+                    </form>
+                )}
+            </div>
+        </>
+    );
+};
+
+export default UserForm;
